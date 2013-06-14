eced3410311be9dd4517779c64cbad29bca8d7a5
Manuel Kauers <mkauers@risc.jku.at>
2013-06-14 11:51:56 +0200

    added files for creating an spkg file.
    To produce an spkg file from the current *.py files, go to the
    spkg directory and type "make". This produces a file ore_algebra-0.1.spkg.
    
    To install the package into your Sage distribution, type
    
      sage -i ore_algebra-0.1.spkg
    
    After that, from within Sage, the package can be loaded via
    
      sage: from ore_algebra import *
    
    --
    
    removed redundant *.py files in doc directory.
    
    To build documentation, go to doc and type:
    
      sage -sh
      cp ../*.py .
      make html
      exit
    
    (So the py files are still needed, but there is no point to have them
    under version control.)

---
+++ spkg/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+    name = "ore_algebra",
+    version = "1.0",
+    author = "Manuel Kauers, Maximilian Jaroschek, Fredrik Johansson",
+    author_email = "manuel@kauers.de",
+    licence = "GPL",
+    packages = ["ore_algebra"]
+    )
+    
