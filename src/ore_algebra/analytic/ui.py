0293500d9571ac27df00c0d137b9979310f329e6
Marc Mezzarobba <marc@mezzarobba.net>
2015-09-04 14:08:16 +0200

    start adding some tests

--- src/ore_algebra/analytic/ui.py
+++ src/ore_algebra/analytic/ui.py
@@ -4,11 +4,79 @@ Some convenience functions for direct use of the features of this package
 
 Ultimately, the typical way to use it should be through methods of objects such
 as differential operators and D-finite functions, not through this module!
+
+
+FIXME: silence deprecation warnings::
+
+    sage: def ignore(*args): pass
+    sage: sage.misc.superseded.warning=ignore
+
+EXAMPLES::
+
+    sage: from ore_algebra import *
+    sage: from ore_algebra.analytic.ui import *
+    sage: QQi.<i> = QuadraticField(-1)
+    sage: Pol.<x> = QQ[]
+    sage: Dop.<Dx> = OreAlgebra(Pol)
+
+    sage: dop = (x^2 + 1)*Dx^2 + 2*x*Dx
+
+    sage: eval_diffeq(dop, [0, 1], [0, 1+i], 1e-30)
+    1.01722196789785136772278896... + 0.40235947810852509365018983...*I
+
+    sage: transition_matrix(dop, [0, 1+i], 1e-10)
+    [                              1 1.017221967... + 0.402359478...*I]
+    [                              0 0.200000000... - 0.400000000...*I]
+
+Return the values (resp. transition matrices) corresponding to several points
+along the path at once::
+
+    sage: multi_eval_diffeq(dop, [0, 1], [k/5 for k in range(5)], 1e-10)
+    [(0, 0),
+    (1/5, 0.19739555985...),
+    (2/5, 0.3805063771...),
+    (3/5, 0.5404195002...),
+    (4/5, 0.674740942...)]
+
+    sage: tms = transition_matrices(dop, [k/5 for k in range(5)], 1e-10)
+    sage: tms[2]
+    (
+        [             1 0.3805063771...]
+    2/5, [             0 0.8620689655...]
+    )
+
+
 """
 
-from . import analytic_continuation as ancont
+# NOTE: to run the tests, use something like
+#
+#     SAGE_PATH="$PWD" sage -t ore_algebra/analytic/ui.py
+
+# from . import analytic_continuation as ancont
+from ore_algebra.analytic import analytic_continuation as ancont
 
 def transition_matrix(dop, path, eps):
+    """
+    EXAMPLES::
+
+        sage: from ore_algebra import *
+        sage: from ore_algebra.analytic.ui import *
+        sage: QQi.<i> = QuadraticField(-1)
+        sage: Pol.<x> = QQ[]
+        sage: Dop.<Dx> = OreAlgebra(Pol)
+
+        sage: dop = (x^2 + 1)*Dx^2 + 2*x*Dx
+
+        sage: transition_matrix(dop, [0, 1], 1e-20)
+        [                      1 0.7853981633974483096...]
+        [                      0 0.5000000000000000000...]
+
+        sage: transition_matrix(dop, [0, 1+i], 1e-10)
+        [                              1 1.017221967... + 0.402359478...*I]
+        [                              0 0.200000000... - 0.400000000...*I]
+
+
+    """
     ctx = ancont.Context(dop, path, eps)
     pairs = ancont.analytic_continuation(ctx)
     assert len(pairs) == 1
