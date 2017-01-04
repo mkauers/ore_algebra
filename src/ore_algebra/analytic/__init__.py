e19834921f533812b1962519f701be29885369b6
Marc Mezzarobba <marc@mezzarobba.net>
2017-01-04 12:10:58 +0100

    update comment on how to run the tests

--- src/ore_algebra/analytic/__init__.py
+++ src/ore_algebra/analytic/__init__.py
@@ -7,5 +7,5 @@ TODO: add general information about the subpackage here
 
 # NOTE: to run the tests, use something like
 #
-#     SAGE_PATH="$PWD" sage -t ore_algebra/analytic/
+#     SAGE_PATH="$PWD" sage -t --force-lib ore_algebra/analytic/
 
