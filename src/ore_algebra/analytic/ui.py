4ed3a9fac2c259187f1fe33ca883313ee67dd146
Marc Mezzarobba <marc@mezzarobba.net>
2017-01-05 17:17:54 +0100

    get rid of superseded features from oaa.ui

--- src/ore_algebra/analytic/ui.py
+++ src/ore_algebra/analytic/ui.py
@@ -1,50 +1,28 @@
 # -*- coding: utf-8 - vim: tw=80
 """
-Some convenience functions for direct use of the features of this package
-
-Ultimately, the typical way to use it should be through methods of objects such
-as differential operators and D-finite functions, not through this module!
-
-EXAMPLES::
-
-    sage: from ore_algebra.analytic.ui import *
-    sage: Dops, x, Dx = Diffops()
-
-Return the values (resp. transition matrices) corresponding to several points
-along the path at once::
-
-    sage: dop = (x^2 + 1)*Dx^2 + 2*x*Dx
-    sage: multi_eval_diffeq(dop, [0, 1], [k/5 for k in range(5)], 1e-10)
-    [(0, 0),
-    (1/5, [0.197395559...]),
-    (2/5, [0.380506377...]),
-    (3/5, [0.540419500...]),
-    (4/5, [0.674740942...])]
-
-    sage: tms = transition_matrices(dop, [k/5 for k in range(5)], 1e-10)
-    sage: tms[2]
-    (
-         [ 1.0... [0.3805063771...]]
-    2/5, [      0 [0.8620689655...]]
-    )
-
+Some convenience functions for features not yet easily accessible from methods
+of differential operators.
 """
 
+from ..ore_algebra import DifferentialOperators
 from . import analytic_continuation as ancont
 from . import polynomial_approximation as polapprox
-
-def transition_matrix(dop, path, eps=1e-16):
-    ctx = ancont.Context(dop, path, eps)
-    pairs = ancont.analytic_continuation(ctx)
-    assert len(pairs) == 1
-    return pairs[0][1]
 
 def transition_matrices(dop, path, eps=1e-16):
     r"""
     EXAMPLES::
 
         sage: from ore_algebra.analytic.ui import *
-        sage: Dops, x, Dx = Diffops()
+        sage: Dops, x, Dx = DifferentialOperators()
+
+        sage: dop = (x^2 + 1)*Dx^2 + 2*x*Dx
+        sage: tms = transition_matrices(dop, [k/5 for k in range(5)], 1e-10)
+        sage: tms[2]
+        (
+            [ 1.0... [0.3805063771...]]
+        2/5, [      0 [0.8620689655...]]
+        )
+
         sage: transition_matrices(Dx - 1, [i/5 for i in range(6)], 1e-10)
         [(0,   [1.000000000...]),
          (1/5, [[1.221402758...]]),
@@ -63,20 +41,21 @@ def _value_from_mat(mat):
     else:
         return mat.base_ring().zero()
 
-def eval_diffeq(dop, ini, path, eps=1e-16):
-    ctx = ancont.Context(dop, path, eps)
-    pairs = ancont.analytic_continuation(ctx, ini=ini)
-    assert len(pairs) == 1
-    _, mat = pairs[0]
-    return _value_from_mat(mat)
-
 def multi_eval_diffeq(dop, ini, path, eps=1e-16):
     """
     EXAMPLES::
 
         sage: from ore_algebra.analytic.ui import *
-        sage: Dops, x, Dx = Diffops()
+        sage: Dops, x, Dx = DifferentialOperators()
         sage: QQi.<i> = QuadraticField(-1, 'I')
+
+        sage: dop = (x^2 + 1)*Dx^2 + 2*x*Dx
+        sage: multi_eval_diffeq(dop, [0, 1], [k/5 for k in range(5)], 1e-10)
+        [(0, 0),
+        (1/5, [0.197395559...]),
+        (2/5, [0.380506377...]),
+        (3/5, [0.540419500...]),
+        (4/5, [0.674740942...])]
 
     The logarithm::
 
@@ -98,27 +77,3 @@ def multi_eval_diffeq(dop, ini, path, eps=1e-16):
 
 polynomial_approximation_on_disk = polapprox.on_disk
 polynomial_approximation_on_interval = polapprox.on_interval
-
-def make_proc(xxx): # ???
-    pass
-
-def Diffops(sx='x'):
-    """
-    Return the Ore algebra of differential operators with polynomial
-    coefficients over ℚ, along with objects representing, x and d/dx
-
-    EXAMPLE::
-
-        sage: from ore_algebra.analytic.ui import *
-        sage: Dops, x, Dx = Diffops()
-        sage: Dops
-        Univariate Ore algebra in Dx over Univariate Polynomial Ring in x over Rational Field
-        sage: x*Dx + 1
-        x*Dx + 1
-    """
-    from sage.rings.rational_field import QQ
-    from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
-    from ore_algebra.ore_algebra import OreAlgebra
-    Pol, x = PolynomialRing(QQ, sx).objgen()
-    Dop, Dx = OreAlgebra(Pol, 'D' + sx).objgen()
-    return Dop, x, Dx
