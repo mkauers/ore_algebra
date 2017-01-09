mkauers/ore_algebra

commit b6e8bfa4704c09156bffd916436797c71e06e00c

naive_sum: fix regressions for recs of order zero

src/ore_algebra/analytic/naive_sum.py
@@ -468,7 +468,7 @@ def series_sum_ordinary(Intervals, dop, bwrec, ini, pt,
         if n%stride == 0:
             done, tail_bound = check_convergence(tail_bound)
             if done: break
-        bwrec_n = bwrec_nplus[0]
+        bwrec_n = bwrec.eval_int_ball(Intervals, n)
         comb = sum(bwrec_n[k]*last[k] for k in xrange(1, ordrec+1))
         last[0] = -~bwrec_n[0]*comb
         # logger.debug("n = %s, [c(n), c(n-1), ...] = %s", n, list(last))

src/ore_algebra/analytic/bounds.py
@@ -1432,8 +1432,8 @@ def residual(n, bwrec_nplus, last, z):
         sage: (Dt^2 + 1).to_T('Tt')(trunc).change_ring(CBF)
         ([0.041666...])*t^6 + ([-0.16666...])*t^5
     """
-    ordrec = len(bwrec_nplus[0]) - 1
-    assert len(bwrec_nplus) == ordrec
+    ordrec = len(bwrec_nplus)
+    assert ordrec == 0 or ordrec == len(bwrec_nplus[0]) - 1
     rescoef = [
         sum((bwrec_nplus[i][i+k+1])*IC(last[k])
             for k in xrange(ordrec-i))

commit 00cacdc9f9a68214620a0f55ce5fe99f8237e14e

naive_sum: fix speed regression from b6e8bfa4704c09156bffd916436797c71e06e00c

src/ore_algebra/analytic/naive_sum.py
@@ -469,7 +469,8 @@ def series_sum_ordinary(Intervals, dop, bwrec, ini, pt,
         if n%stride == 0:
             done, tail_bound = check_convergence(tail_bound)
             if done: break
-        bwrec_n = bwrec.eval_int_ball(Intervals, n)
+        bwrec_n = (bwrec_nplus[0] if bwrec_nplus
+                   else bwrec.eval_int_ball(Intervals, n))
         comb = sum(bwrec_n[k]*last[k] for k in xrange(1, ordrec+1))
         last[0] = -~bwrec_n[0]*comb
         # logger.debug("n = %s, [c(n), c(n-1), ...] = %s", n, list(last))