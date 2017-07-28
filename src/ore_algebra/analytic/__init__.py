1a2e53a898a32fb53506d2bf010f8359b04f9893
Marc Mezzarobba <marc@mezzarobba.net>
2017-07-28 15:23:06 +0200

    Add a test...
    I made a note long ago that this example should be in my test suite; I
    don't remember why.

--- src/ore_algebra/analytic/__init__.py
+++ src/ore_algebra/analytic/__init__.py
@@ -231,6 +231,12 @@ Operators with rational function coefficients::
     [[0.0340875989376363...]   [1.59063685463732...]]
     [[-0.579827135138349...]   [2.27958530233606...]]
 
+Miscellaneous tests::
+
+    sage: dop =  -452*Dx^10 + (-2*x^2 - x - 1/2)*Dx^9 + (1/2*x + 22)*Dx^8 + (1/4*x^2 + x)*Dx^7 + (1/3*x^2 - 1/2*x + 1/3)*Dx^6 + (-3*x^2 + x + 1)*Dx^5 + (x^2 - 4/3*x)*Dx^4 + (2*x^2 - 2*x)*Dx^3 + (2*x^2 + x)*Dx^2 + (-2/3*x^2 - 5/27*x - 1/3)*Dx - 18*x^2 + 6/5*x - 6
+    sage: ((dop.numerical_transition_matrix([0,1])*dop.numerical_transition_matrix([1, 1+i, 0]) - 1)).norm('frob') < 1e-13
+    True
+
 Test suite
 ==========
 
