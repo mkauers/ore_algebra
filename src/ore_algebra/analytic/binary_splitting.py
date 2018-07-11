23d605f7e35220eafce2cbe58ce13806ea3fdc79
Marc Mezzarobba <marc@mezzarobba.net>
2018-07-11 18:00:54 +0200

    binsplit: switch to a simpler representation...
    ...for the simultaneous computation of several solutions
    
    The "clever" representation/indexing convention I was using before,
    besides having issues when the order of the recurrence is larger than
    that of the differential equation, doesn't work the regular singular
    case.
    
    (The new code is currently a bit slower than the old one, but let's keep
    that issue for later.)

--- src/ore_algebra/analytic/binary_splitting.py
+++ src/ore_algebra/analytic/binary_splitting.py
@@ -36,15 +36,15 @@ TESTS::
     sage: ((x + 1)*Dx^3 + Dx^2).numerical_transition_matrix([0,1/2], algorithm='binsplit')
     INFO:ore_algebra.analytic.binary_splitting:...
     [ [1.000000000000000...] [0.5000000000000000...] [0.2163953243244931...]]
-    [                      0  [1.000000000000000...] [0.8109302162163287...]]
-    [                      0                       0 [0.6666666666666666...]]
+    [              [+/- ...]  [1.000000000000000...] [0.8109302162163287...]]
+    [              [+/- ...]               [+/- ...] [0.6666666666666666...]]
 
     sage: (Dx - 1).numerical_solution([1], [0, i + pi], algorithm="binsplit")
     INFO:ore_algebra.analytic.binary_splitting:...
     [12.5029695888765...] + [19.4722214188416...]*I
 
     sage: from ore_algebra.examples import fcc
-    sage: fcc.dop5.numerical_solution( # long time (6.7 s)
+    sage: fcc.dop5.numerical_solution( # long time (7.2 s)
     ....:          [0, 0, 0, 0, 1, 0], [0, 1/5+i/2, 1],
     ....:          1e-60, algorithm='binsplit')
     INFO:ore_algebra.analytic.binary_splitting:...
@@ -55,17 +55,17 @@ TESTS::
     sage: from ore_algebra.analytic.binary_splitting import MatrixRec
 
     sage: rec = MatrixRec((x-2)*Dx^3 - 1, 0, RBF(1/3), 3, 10)
-    sage: rec.partial_sums(rec.binsplit(0, 10), RBF, 3)
-    [ [0.996...]  [0.333...] [0.111...]]
-    [[-0.029...]  [0.996...] [0.666...]]
-    [[-0.091...] [-0.015...] [0.996...]]
+    sage: rec.partial_sums([rec.binsplit(2, 7)*rec.ordinary_ini(2,3)], RBF, 3)
+    [[0.1110739597...]]
+    [ [0.666100823...]]
+    [ [0.996527777...]]
 
     sage: QQi.<i> = QuadraticField(-1)
     sage: rec = MatrixRec((x-i)*Dx^3 - 1, 0, RBF(1/3), 3, 10)
-    sage: rec.partial_sums(rec.binsplit(0, 10), CBF, 3)
-    [ [1.0005...] + [0.0061...]*I [0.3333...] + [0.0005...]*I [0.1111...] + [6.6513...]*I]
-    [[0.0059...]  + [0.0545...]*I [1.0009...] + [0.0059...]*I [0.6668...] + [0.0009...]*I]
-    [ [0.0261...] + [0.1609...]*I [0.0057...] + [0.0263...]*I [1.0014...] + [0.0057...]*I]
+    sage: rec.partial_sums([rec.binsplit(2, 7)*rec.ordinary_ini(0,3)], CBF, 3)
+    [[1.0005...] + [0.0061...]*I]
+    [[0.0059...] + [0.0545...]*I]
+    [  [0.02...] +  [0.160...]*I]
 """
 
 import copy
@@ -149,7 +149,7 @@ class StepMatrix_generic(StepMatrix):
         # MatrixMatrixAction once and for all.
         low.rec_mat = high.rec_mat._multiply_classical(low.rec_mat) # Mat(rec_Ints)
         tmp = high.sums_row._multiply_classical(mat) # Vec(sums_Ints[[δ]]/<δ^k>)
-        for i in xrange(mat.nrows()):
+        for i in xrange(mat.ncols()):
             tmp[0,i] = tmp[0,i]._mul_trunc_(low.BigScalars(low.pow_num), low.ord)
         #assert tmp[0][0].degree() < low.ord
         tmp2 = high.rec_den * high.pow_den
@@ -176,10 +176,10 @@ class StepMatrix_arb(StepMatrix):
         # prod.sums_row = high.sums_row * low.rec_mat * low.pow_num + O(δ^ord)
         #               + high.rec_den * high.pow_den * low.sums_row
         tmp = [high.sums_row[0,j]._mul_trunc_(low.pow_num, low.ord)
-               for j in range(high.sums_row.ncols())]
+               for j in xrange(ordrec)]
         low.sums_row = low.sums_row._lmul_(high.rec_den * high.pow_den)
         # XXX try converting tmp to an ord×ordrec matrix instead?
-        for j in xrange(ordrec):
+        for j in xrange(low.rec_mat.ncols()):
             for i in xrange(ordrec):
                 low.sums_row[0,j] += low.rec_mat[i,j]*tmp[i]
         # With moderately large matrices, all the time goes in the following
@@ -199,36 +199,28 @@ class MatrixRec(object):
     Note: Mathematically, the recurrence matrix has the structure of a
     StepMatrix (depending on parameters). However, this class does not
     derive from StepMatrix as the data structure is different.
+
+    Conventions:
+
+        [ u(n-s+1)·z^n ]   [ 0 1     |   ]  [ u(n-s)·z^n ]
+        [      ⋮       ]   [     ⋱   |   ]  [     ⋮      ]
+        [              ]   [       1 | 0 ]  [            ]
+        [ u(n)    ·z^n ] = [ * * ⋯ * |   ]  [ u(n-1)·z^n ]
+        [ ------------ ]   [ --------+-- ]  [ ---------- ]
+        [     σ(n)     ]   [ 0 ⋯ 0 1 | 1 ]  [   σ(n-1)   ]
+
+              U(n)       =      B(n)            U(n-1)
+
+        U(n) = B(n)···B(1)·U(0)
     """
 
     def __init__(self, diffop, shift, dz, derivatives, nterms_est):
 
-        #### Recurrence operator & matrix
-
+        # Recurrence operator & matrix
         bwrec = bw_shift_rec(diffop, clear_denominators=True)
-
-        # It can happen that the recop (after normalization to Sn-valuation 0)
-        # has order < orddeq, and our strategy of using vectors of coefficients
-        # of the form [u(n-s'), ..., u(n+r-1)] with s'=s-r does not work in this
-        # case. In this case, we artificially extend the recurrence matrix to
-        # something of the form
-        #
-        #    [ 0 1         ]
-        #    [     1       ]
-        #    [       …     ]  ∈ K[n]^(ordmat×ordmat)
-        #    [         1   ]    (K = Frac(AlgInts_rec))
-        #    [           1 ]
-        #    [ 0 … 0 * … * ]  <-- ordrec nonzeros
-        #
-        # (This may change with the extension to regular singular points.)
-
         self.ordrec = bwrec.order
-        self.orddeq = diffop.order()
-        ordmat = max(self.ordrec, self.orddeq)
-        self.orddelta = ordmat - self.orddeq
 
-        #### Choose computation domains
-
+        # Choose computation domains
         deq_Scalars = diffop.base_ring().base_ring()
         E = dz.parent()
         assert deq_Scalars is E or deq_Scalars != E
@@ -238,30 +230,28 @@ class MatrixRec(object):
             # or ZZ[i]. Round the entries that are larger than the target
             # precision (+ some guard digits) in the upper levels of the tree.
             # Choice of working precision TBI.
-            prec = 8 + nterms_est*(1 + ZZ(ZZ(ordmat).nbits()).nbits())
+            prec = 8 + nterms_est*(1 + ZZ(ZZ(self.ordrec).nbits()).nbits())
             self._init_CBF(deq_Scalars, E, dz, prec)
         else:
             self._init_generic(deq_Scalars, E, dz)
 
         self.bwrec = bwrec.change_base(self.AlgInts_rec)
-        self.Mat_rec = MatrixSpace(self.AlgInts_rec, ordmat, ordmat)
+        self.Mat_rec = MatrixSpace(self.AlgInts_rec, self.ordrec, self.ordrec)
 
         assert self.bwrec[0].base_ring() is self.AlgInts_rec # uniqueness
         assert self.bwrec[0](0).parent() is self.AlgInts_rec #   issues...
 
-        #### Power of dz. Note that this part does not depend on n.
-
+        # Power of dz. Note that this part does not depend on n.
         Series_pow = PolynomialRing(self.AlgInts_pow, 'delta')
         self.pow_num = Series_pow([self.pow_den*dz, self.pow_den])
         self.derivatives = derivatives
 
-        #### Partial sums
-
+        # Partial sums
         self.Series_sums = PolynomialRing(self.AlgInts_sums, 'delta')
         self.series_class_sums = type(self.Series_sums.gen())
         assert self.Series_sums.base_ring() is self.AlgInts_sums
 
-        self.Mat_sums_row = MatrixSpace(self.Series_sums, 1, ordmat)
+        self.Mat_sums_row = MatrixSpace(self.Series_sums, 1, self.ordrec)
         self.Mat_series_sums = self.Mat_rec.change_ring(self.Series_sums)
 
     def _init_CBF(self, deq_Scalars, E, dz, prec):
@@ -323,14 +313,14 @@ class MatrixRec(object):
 
     def __call__(self, n):
         stepmat = self.StepMatrix_class()
-        stepmat.idx_start = n
-        stepmat.idx_end = n + 1
-        stepmat.rec_den = self.bwrec[0](n + self.orddeq)
+        stepmat.idx_start = n - 1
+        stepmat.idx_end = n
+        stepmat.rec_den = self.bwrec[0](n)
         stepmat.rec_mat = self.Mat_rec.matrix()
         for i in xrange(self.Mat_rec.nrows() - 1):
             stepmat.rec_mat[i, i+1] = stepmat.rec_den
         for i in xrange(self.ordrec):
-            stepmat.rec_mat[-1, -1-i] = -self.bwrec[i+1](n + self.orddeq)
+            stepmat.rec_mat[-1, -1-i] = -self.bwrec[i+1](n)
         stepmat.pow_num = self.pow_num
         stepmat.pow_den = self.pow_den
         # TODO: fix redundancy--the rec_den*pow_den probabably doesn't belong
@@ -339,7 +329,7 @@ class MatrixRec(object):
         den = stepmat.rec_den * stepmat.pow_den
         den = self.series_class_sums(self.Series_sums, [den])
         stepmat.sums_row = self.Mat_sums_row.matrix()
-        stepmat.sums_row[0, self.orddelta] = den
+        stepmat.sums_row[0, -1] = den
         stepmat.ord = self.derivatives
 
         stepmat.BigScalars = self.Series_sums # XXX unused in arb case
@@ -362,25 +352,48 @@ class MatrixRec(object):
 
         return stepmat
 
+    def ordinary_ini(self, i, orddeq): # temporary
+        stepmat = self.StepMatrix_class()
+        stepmat.idx_start = i
+        stepmat.idx_end = orddeq - 1
+        stepmat.rec_mat = matrix(self.AlgInts_rec, self.ordrec, 1)
+        if orddeq - i <= self.ordrec:
+            stepmat.rec_mat[-orddeq+i] = 1
+        stepmat.rec_den = self.bwrec[0].base_ring().one()
+        stepmat.pow_num = self.pow_num.power_trunc(orddeq - 1, self.derivatives)
+        stepmat.pow_den = self.pow_den**(orddeq - 1)
+        stepmat.sums_row = matrix(self.Series_sums, 1, 1,
+                self.pow_num.power_trunc(i, self.derivatives)
+                        * self.pow_den**(orddeq-1-i) if i < orddeq - 1 else 0)
+        stepmat.ord = self.derivatives
+
+        stepmat.BigScalars = self.Series_sums # XXX unused in arb case
+        stepmat.Mat_big_scalars = self.Mat_series_sums
+
+        return stepmat
+
     def binsplit(self, low, high):
+        r"""
+        Compute R(high)·R(high-1)···R(low+1) by binary splitting.
+        """
         if high - low <= self.StepMatrix_class.binsplit_threshold:
             mat = self.one(low)
-            for n in xrange(low, high):
+            for n in xrange(low + 1, high + 1):
                 mat.imulleft(self(n))
         else:
             mid = (low + high) // 2
             mat = self.binsplit(low, mid)
             mat.imulleft(self.binsplit(mid, high))
+        assert mat.idx_start == low and mat.idx_end == high
         return mat
 
     def __repr__(self):
         return pprint.pformat(self.__dict__)
 
-    # XXX: needs testing
-    def normalized_residual(self, maj, prod, n, j):
+    def normalized_residual(self, maj, prod, n):
         r"""
         Compute the normalized residual associated with the fundamental
-        solution of index j.
+        solution.
 
         TESTS::
 
@@ -390,55 +403,43 @@ class MatrixRec(object):
             sage: ode.numerical_transition_matrix([0,1+I,1], 1e-100, algorithm='binsplit')
             [[0.707...2078...] + [0.707...]*I]
         """
-        r, s = self.orddeq, self.ordrec
+        # WARNING: this residual must correspond to the operator stored in
+        # maj.dop, which typically isn't self.diffop (but an operator in θx
+        # equal to x^k·self.diffop for some k).
         IC = bounds.IC
-        # Compute the "missing" coefficients u(n-s), ..., u(n-s'-1) s'=s-r):
-        # indeed, it is convenient to compute the residuals starting from
-        # u(n-s), ..., u(n-1), while our recurrence matrices produce the partial
-        # sum of index n along with the vector [u(n-s'), ..., u(n+r-1)].
-        last = [IC.zero()]*r  # u(n-s), ..., u(n-s'-1)
-        last.extend([IC(c)/IC(prod.rec_den)             # u(n-s'), ..., u(n+r-1)
-                     for c in prod.rec_mat.column(s-r+j)])  # XXX: check column index
-        for i in xrange(r-1, -1, -1): # compute u(n-s+i)
-            last[i] = ~(self.bwrec[s](n+i))*sum(self.bwrec[s-k](n+i)*last[i+k]
-                                                for k in xrange(1, s+1))
-        # Now compute the residual. WARNING: this residual must correspond to
-        # the operator stored in maj.dop, which typically isn't self.diffop (but
-        # an operator in θx equal to x^k·self.diffop for some k).
+        last = [[IC(c)/IC(prod.rec_den)]                 # [u(n-1), ..., u(n-s)]
+                for c in reversed(prod.rec_mat.column(-1))]
         # XXX: do not recompute this every time!
-        bwrnp = [[[pol(n + i)] for pol in self.bwrec] for i in range(s)]
-        altlast = [[c] for c in reversed(last[:s])]
-        return maj.normalized_residual(n, altlast, bwrnp)
+        bwrnp = [[[pol(n + i)] for pol in self.bwrec]
+                 for i in range(self.ordrec)]
+        return maj.normalized_residual(n, last, bwrnp)
 
-    def normalized_residuals(self, maj, prod, n):
-        return [self.normalized_residual(maj, prod, n, j)
-                for j in xrange(self.orddeq)]
-
-    def term(self, prod, parent, j):
+    def term(self, prod, ring, j):
         r"""
-        Given a matrix representing a product B(n-1)···B(0) where B is the
-        recurrence matrix associated to some differential operator P, return the
-        term of index n of the fundamental solution of P of the form
+        Given a matrix representing a product B(n)···B(1) where B is the
+        backward recurrence matrix (U(n) = B(n)·U(n-1)) associated to some
+        differential operator P, return the term of index n of the fundamental
+        solution of P of the form
         y[j](z) = z^j + O(z^r), 0 <= j < r = order(P).
         """
-        orddelta = self.orddelta
-        num = parent(prod.rec_mat[orddelta + j, orddelta])*parent(prod.pow_num[0])
-        den = parent(prod.rec_den)*parent(prod.pow_den)
+        num = ring(prod.rec_mat[-1, -1])*ring(prod.pow_num[0])
+        den = ring(prod.rec_den)*ring(prod.pow_den)
         return num/den
 
-    def partial_sums(self, prod, ring, rows):
+    def partial_sums(self, prods, ring, rows):
         r"""
-        Return a matrix of partial sums of the series and its derivatives.
+        Return a matrix of partial sums of solutions and their derivatives.
+
+        The result has size rows × len(prods).
         """
-        numer = matrix(ring, rows, self.orddeq,
-                       lambda i, j: prod.sums_row[0, self.orddelta+j][i])
+        coeffs = [prod.sums_row[0,-1].padded_list(rows) for prod in prods]
+        numer = matrix(ring, coeffs).transpose()
+        assert numer.dimensions() == (rows, len(prods))
         denom = ring(prod.rec_den)*ring(prod.pow_den)
         return numer/denom
 
     def error_estimate(self, prod):
-        orddelta = self.orddelta
-        num1 = sum(abs(prod.rec_mat[orddelta + j, orddelta])
-                   for j in range(self.orddeq))
+        num1 = abs(prod.rec_mat[-1, -1])
         num2 = sum(abs(a) for a in prod.pow_num)
         den = abs(prod.rec_den)*abs(prod.pow_den)
         return num1*num2/den
@@ -459,22 +460,30 @@ def fundamental_matrix_ordinary(dop, pt, eps, rows, maj, fail_fast):
     logger.log(logging.INFO - 1, "target error = %s", eps)
     rec = MatrixRec(dop, 0, pt, rows, utilities.prec_from_eps(eps))
     rad = bounds.IC(pt).abs()
-    prod, n, tail_bound = rec.one(0), None, bounds.IR('inf')
+    # Each solution is represented as a column StepMatrix to be applied to the
+    # column vector [ 1 ] to get the coefficients and partial sum of the series.
+    # In the general regular case, we will also need to record a Log shift to be
+    # used in place of the 1 in the vector, and to update that shift when
+    # crossing a singular index.
+    prods, n, tail_bound = [], None, bounds.IR('inf')
     # XXX clarify exact criterion
     stop = accuracy.StoppingCriterion(maj=maj, eps=eps, fast_fail=False)
     def get_residuals():
-        return rec.normalized_residuals(maj, prod, n)
+        return [rec.normalized_residual(maj, mat, n) for mat in prods]
     def get_bound(maj):
         return maj.bound(rad, rows=rows, cols=rows)
-    for last, n in binsplit_step_seq(0):
-        prod = rec.binsplit(last, n) * prod
+    prods = [rec.ordinary_ini(i, dop.order()) for i in range(dop.order())]
+    for last, n in binsplit_step_seq(dop.order() - 1):
+        fwd = rec.binsplit(last, n)
+        for prod in prods:
+            prod.imulleft(fwd)
         done, tail_bound = stop.check(get_bound, get_residuals, None,
                 False, n, tail_bound, rec.error_estimate(prod), next_stride=n)
         if done:
             break
     is_real = utilities.is_real_parent(pt.parent())
     Intervals = utilities.ball_field(eps, is_real)
-    mat = rec.partial_sums(prod, Intervals, rows)
+    mat = rec.partial_sums(prods, Intervals, rows)
     # Account for the dropped high-order terms in the intervals we return.
     err = tail_bound.abs()
     mat = mat.apply_map(lambda x: x.add_error(err)) # XXX - overest
