a45982cade5ed73eabfc0e65ba82ce4063bab069
Marc Mezzarobba <marc@mezzarobba.net>
2019-09-20 17:42:52 +0200

    speed up evaluation of polynomials over ℚ(✓d)
    ...in cases other than ℚ[i], which was taken care of before

--- src/ore_algebra/analytic/eval_poly_at_int.pyx
+++ src/ore_algebra/analytic/eval_poly_at_int.pyx
@@ -14,6 +14,7 @@ from sage.libs.flint.fmpz_poly cimport *
 from sage.libs.gmp.mpz cimport *
 
 from sage.rings.complex_arb cimport ComplexBall
+from sage.rings.integer cimport Integer
 from sage.rings.number_field.number_field_element_quadratic cimport NumberFieldElement_quadratic
 from sage.rings.polynomial.polynomial_complex_arb cimport Polynomial_complex_arb
 from sage.rings.polynomial.polynomial_element cimport Polynomial
@@ -40,40 +41,79 @@ def cbf(pol, n, tgt):
 
     return res
 
-cdef NumberFieldElement_quadratic _qnf(pol, n):
+cdef void _qnf(mpz_t a, mpz_t b, Polynomial pol, n):
 
-    cdef Polynomial _pol = (<Polynomial> pol)
     cdef unsigned long _n = PyInt_AsLong(n)
     cdef unsigned long i
 
-    cdef NumberFieldElement_quadratic res
-    res = (<NumberFieldElement_quadratic> (<Ring> _pol._parent._base)._zero_element)._new()
-    mpz_set_ui(res.a, 0)
-    mpz_set_ui(res.b, 0)
-    mpz_set_ui(res.denom, 1)
-
     cdef NumberFieldElement_quadratic c
 
+    mpz_set_ui(a, 0)
+    mpz_set_ui(b, 0)
+
     for i in range(pol.degree(), -1, -1):
-        mpz_mul_ui(res.a, res.a, _n)
-        mpz_mul_ui(res.b, res.b, _n)
-        c = _pol.get_unsafe(i)
+        mpz_mul_ui(a, a, _n)
+        mpz_mul_ui(b, b, _n)
+        c = pol.get_unsafe(i)
         # In general, quadratic algebraic integers can have c != 1, but the way
         # we choose the generators of our quadratic number fields should avoid
         # that.
         assert mpz_cmp_ui(c.denom, 1) == 0
-        mpz_add(res.a, res.a, c.a)
-        mpz_add(res.b, res.b, c.b)
-
-    return res
+        mpz_add(a, a, c.a)
+        mpz_add(b, b, c.b)
 
 def qnf(pol, n, tgt):
-    assert tgt is (<Polynomial> pol)._parent._base
-    return _qnf(pol, n)
+    cdef Polynomial _pol = (<Polynomial> pol)
+    assert tgt is _pol._parent._base
+
+    cdef NumberFieldElement_quadratic res
+    res = (<NumberFieldElement_quadratic> (<Ring> _pol._parent._base)._zero_element)._new()
+    mpz_set_ui(res.denom, 1)
+    _qnf(res.a, res.b, _pol, n)
+    return res
 
 def qnf_to_cbf(pol, n, tgt):
-    cdef NumberFieldElement_quadratic val = _qnf(pol, n)
-    return val._acb_(tgt)
+    # Adapted from the implementation of NumberFieldElement_quadratic._a[rc]_ in Sage.
+    cdef Polynomial _pol = <Polynomial> pol
+    cdef Integer D = <Integer> _pol._parent._base._D
+    cdef bint standard_embedding = _pol._parent._base._standard_embedding
+
+    cdef mpz_t a, b
+    mpz_init(a)
+    mpz_init(b)
+    _qnf(a, b, _pol, n)
+
+    cdef ComplexBall res = ComplexBall.__new__(ComplexBall)
+    res._parent = tgt
+    cdef fmpz_t tmp
+    fmpz_init(tmp)
+
+    fmpz_set_mpz(tmp, a)
+    arb_set_fmpz(acb_realref(res.value), tmp)
+
+    fmpz_set_mpz(tmp, D.value)
+    cdef long prec = tgt._prec
+    cdef long prec2 = prec
+    if mpz_sgn(D.value) < 0:
+        fmpz_neg(tmp, tmp)
+    if mpz_sgn(a)*mpz_sgn(b) > 0 ^ standard_embedding:
+        # we expect a lot of cancellation in the subtraction that follows
+        prec2 = max(prec, <long> mpz_sizeinbase(b, 2))
+    arb_sqrt_fmpz(acb_imagref(res.value), tmp, prec2)
+    if not standard_embedding:
+        arb_neg(acb_imagref(res.value), acb_imagref(res.value))
+
+    fmpz_set_mpz(tmp, b)
+    if mpz_sgn(D.value) < 0:
+        arb_mul_fmpz(acb_imagref(res.value), acb_imagref(res.value), tmp, prec)
+    else:
+        arb_addmul_fmpz(acb_realref(res.value), acb_imagref(res.value), tmp, prec)
+        arb_zero(acb_imagref(res.value))
+
+    fmpz_clear(tmp)
+    mpz_clear(b)
+    mpz_clear(a)
+    return res
 
 @cython.boundscheck(False)
 def qqi_to_cbf(zzpols, n, tgt):
